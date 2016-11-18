e3d4a6c2c328c085dd4259f895a06edf14c04c1a
aarohmankad <aarohmankad@gmail.com>
2016-11-17 21:46:46 -0800

    nested classes, FriendsContainer and ShowList

--- app/index.js
+++ app/index.js
@@ -2,15 +2,38 @@ var
 	React = require('react'),
 	ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var FriendsContainer = React.createClass({
 	render: function() {
+		var name = this.props.name;
+		var friends = ['Cristian', 'Elijah', 'Sean', 'Alex', 'Peter'];
+
 		return (
-			<div> Hello World! </div>
+			<div class='container'>
+				<h1> Hey {this.props.name}! </h1>
+				<ShowList elements={friends} />
+			</div>
+		)
+	}
+});
+
+var ShowList = React.createClass({
+	render: function() {
+		var elements = this.props.elements.map(function(element) {
+			return <li> { element } </li>;
+		});
+
+		return (
+			<div>
+				<h3> Friends: </h3>
+				<ul>
+					{ elements }
+				</ul>
+			</div>
 		)
 	}
 });
 
 ReactDOM.render(
-	<HelloWorld/>,
+	<FriendsContainer name="Aaroh"/>,
 	document.getElementById('app')
 );
