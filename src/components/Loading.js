940607589c4de68a1a998f8a1f6f61ada987139a
aarohmankad <aarohmankad@gmail.com>
2016-11-25 12:44:53 -0800

    add default text for loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,13 +3,14 @@ import React from 'react'
 export default React.createClass({
 	getDefaultProps() {
 		return {
-			isLoading: true
+			isLoading: true,
+			content: 'Loading!'
 		}
 	},
 	render() {
 		return (
 			<div className='col-sm-12 text-center'>
-				<h2> Loading! </h2>
+				<h2>{ this.props.content }</h2>
 			</div>
 		)
 	}
